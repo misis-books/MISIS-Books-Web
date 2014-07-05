8bbe3c5d558565b02602d1274b9ad593c11c3771
Alex <ipritoflex@yandex.ru>
2014-07-05 23:29:20 +0400

    Revert "Delete ignore file"
    This reverts commit 0486125882431283d1616c9754af2311214136f1.

--- application/config/db.config.php
+++ application/config/db.config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'host' => 'localhost',
-    'user' => 'u5658416_omlid',
-    'password' => 'QkFsZVgyMDAyNjYxNDAwMDA1YWE=',
-    'database' => 'u5658416_default'
+    'user' => '',
+    'password' => '',
+    'database' => ''
 );
