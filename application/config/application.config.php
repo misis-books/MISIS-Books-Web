5b401ffbf39754f3c02c81032ea0343bb7106ff3
Alex <ipritoflex@yandex.ru>
2014-07-06 15:21:55 +0400

    Added yandex.metrika

--- application/config/application.config.php
+++ application/config/application.config.php
@@ -35,7 +35,8 @@ return array(
                 'charset' => '<meta http-equiv="content-type" content="text/html; charset=utf-8">',
                 'ie' => '<meta http-equiv="X-UA-Compatible" content="IE=edge">',
                 'yandex_tabloid' => '<link rel="yandex-tableau-widget" href="/st/yandex/manifest.json">',
-                'noscript' => '<noscript><meta http-equiv="refresh" content="0; URL=/badbrowser"></noscript>'
+                'noscript' => '<noscript><meta http-equiv="refresh" content="0; URL=/badbrowser"></noscript>',
+                'yandex.metrika' => '<script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter5843680 = new Ya.Metrika({id:5843680, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/5843680" style="position:absolute; left:-9999px;" alt="" /></div></noscript>'
             ),
             'script' => array(
                 'jquery',
