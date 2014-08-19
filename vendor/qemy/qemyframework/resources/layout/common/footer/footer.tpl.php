c2c9d4ca9d1862e85d196f51263748945180e44e
Alex <ipritoflex@yandex.ru>
2014-08-20 03:21:06 +0400

    Change version

--- vendor/qemy/qemyframework/resources/layout/common/footer/footer.tpl.php
+++ vendor/qemy/qemyframework/resources/layout/common/footer/footer.tpl.php
@@ -3,7 +3,7 @@
         <div class="footer_layer">
             <a href="/dev" class="flat_link">MISIS Books API</a>
             <br>
-            <span class="footer_default_text" style="line-height: 160%;"><!--noindex-->MISIS Books v3.5.3<!--/noindex--></span> |
+            <span class="footer_default_text" style="line-height: 160%;"><!--noindex-->MISIS Books v3.6.0<!--/noindex--></span> |
             <a href="http://mini.twosphere.ru" class="flat_link">Мобильная версия</a>
             <a href="https://github.com/ipritoflex/twosphere" target="_blank" class="github-repository"></a>
         </div>
