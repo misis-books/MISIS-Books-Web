c8096464fe32f8ad8319451fb3729338c60f64b6
Alex <ipritoflex@yandex.ru>
2014-08-24 03:12:18 +0400

    Notifications added

--- module/Methods/Controller/Controller.php
+++ module/Methods/Controller/Controller.php
@@ -17,12 +17,14 @@ class Controller extends AbstractController {
         $model = new AddAuthorModel();
         $response = $model->main()->getData();
         echo json_encode($response);
+        $this->sendEmail(1);
     }
 
     public function addEditionAction() {
         $model = new AddEditionModel();
         $response = $model->main()->getData();
         echo json_encode($response);
+        $this->sendEmail(2);
     }
 
     public function getPopularAction() {
@@ -35,11 +37,34 @@ class Controller extends AbstractController {
         $model = new NewTicketModel();
         $response = $model->main()->getData();
         echo json_encode($response);
+        $this->sendEmail(4);
     }
 
     public function searchAction() {
         $model = new SearchModel();
         $response = $model->main()->getData();
         echo json_encode($response);
+    }
+
+    private function sendEmail($type) {
+        $to = 'ipritoflex@yandex.ru';
+        $subject = "No subject";
+        switch($type) {
+            case 1:
+                $subject = '1. [MISIS Books Support] Добавлен новый автор';
+                break;
+            case 2:
+                $subject = '2. [MISIS Books Support] Добавлен новый материал';
+                break;
+            case 4:
+                $subject = '4. [MISIS Books Support] Добавлен новый тикет';
+                break;
+        }
+
+        $message = '<br>http://twosphere.ru/admin<br>';
+        $headers = 'From: admin@twosphere.ru' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        mail($to, $subject, $message, $headers);
     }
 }
